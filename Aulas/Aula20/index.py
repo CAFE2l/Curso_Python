198cf63af1d8a0d540926b0429934323143e8b29
CAFE2l <pciskolargx@gmail.com>
2025-09-12 17:10:34 -0300

    update aulka

---
+++ Aulas/Aula20/index.py
@@ -0,0 +1,12 @@
+def lin():
+    print('-'*30)
+    
+# print('-'*30)
+print("     CURSO EM VÍDEO    ")
+# print('-'*30)
+# print('-'*30)
+print("     APRENDA PYTHON    ")
+# print('-'*30)
+# print('-'*30)
+print("     GUSTAVO GUANABARA    ")
+# print('-'*30)
