bc77269ece0e9fb88a46d3e32cbbc47151620a9b
CAFE2l <pciskolargx@gmail.com>
2025-09-14 11:41:51 -0300

    ex 098 com solução

--- Exercicios/ex098/index.py
+++ Exercicios/ex098/index.py
@@ -61,3 +61,35 @@ def maior(valores):
 
 
 maior(valores=(1,2,3,4, 5, 6, 7, 80))
+
+
+
+
+solution = "SOLUÇÃO DO GUANÁ"
+
+print(f"{estilos['negrito']}{cores['ciano']}{"==="*5}{cores['cinza']}GUANÁ{cores['ciano']}{"==="*5}{cores['limpa']}")
+print(f"{cores['pretoebranco']}{estilos['negrito']}{solution.center(35)}{cores['limpa']}")
+print(f"{estilos['negrito']}{cores['ciano']}{"==="*11+"=="}{cores['limpa']}")
+
+from time import sleep
+
+def maior(* num):
+    cont = maior = 0 
+    print("\nAnalisando os valores passados... ")
+    for valor in num:
+        print(f"{valor} ", end='', flush=True)
+        sleep(0.3)
+        if cont == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+            cont += 1
+    print(f"Foram informados {cont} valores ao todo.")
+    print(f"O maior valor informado foi {maior}.")            
+#Programa principal
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
