727e4072eeada5a35585d23ae3b5d7dd3d8ab19e
CAFE2l <pciskolargx@gmail.com>
2025-09-16 16:10:45 -0300

    ex099 com solução

--- Exercicios/ex099/index.py
+++ Exercicios/ex099/index.py
@@ -100,4 +100,37 @@ sorteia()
 # Chama a segunda função
 somaPar()
 
+
+
+
+solution = "SOLUÇÃO DO GUANÁ"
+
+print(f"{estilos['negrito']}{cores['ciano']}{"==="*5}{cores['cinza']}GUANÁ{cores['ciano']}{"==="*5}{cores['limpa']}")
+print(f"{cores['pretoebranco']}{estilos['negrito']}{solution.center(35)}{cores['limpa']}")
+print(f"{estilos['negrito']}{cores['ciano']}{"==="*11+"=="}{cores['limpa']}")
+
+from random import randint
+from time import sleep
+
+def sorteia(lista):
+    print("Sorteando 5 valores da lista: ", end='')
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f"{n} ", end='', flush=True)
+        sleep(0.3)
+    print("PRONTO!")
+
+def somaPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f"Somando os valores pares de {lista}, temos {soma}")
+
+
+numeros = list()
+sorteia(numeros)
+somaPar(numeros)
+
 print("="*40)
