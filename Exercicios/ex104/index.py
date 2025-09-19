a53799d6bf0149ce2497711c81c7220e6e6d7a5e
CAFE2l <pciskolargx@gmail.com>
2025-09-19 18:51:38 -0300

    ex104 with solution

--- Exercicios/ex104/index.py
+++ Exercicios/ex104/index.py
@@ -1,3 +1,57 @@
+cores = {
+    "limpa": "\033[m",
+    'vermelho': "\033[31m",
+    'verde': "\033[32m",
+    'amarelo': "\033[33m",
+    'azul': "\033[34m",
+    'roxo': "\033[35m",
+    'ciano': "\033[36m",
+    'cinza': "\033[37m",
+    'pretoebranco': '\033[7;30m'
+}
+
+fundo = {
+    "branco": "\033[40m",
+    'vermelho': "\033[41m",
+    'verde': "\033[42m",
+    'amarelo': "\033[43m",
+    'azul': "\033[44m",
+    'roxo': "\033[45m",
+    'ciano': "\033[46m",
+    'cinza': "\033[47m",
+    'vermelho_claro': '\033[101m',
+    'verde_claro': '\033[102m',
+    'amarelo_claro': '\033[103m',
+    'azul_claro': '\033[104m',
+    'roxo_claro': '\033[105m',
+    'ciano_claro': '\033[106m',
+    'cinza_claro': '\033[107m'
+}
+
+estilos = {
+    "reset": "\033[0m",
+    "negrito": "\033[1m",
+    "fraco": "\033[2m",
+    "italico": "\033[3m",
+    "sublinhado": "\033[4m",
+    "inverso": "\033[7m",
+    "invisivel": "\033[8m",
+    "tachado": "\033[9m",
+    "duplosublinhado": "\033[21m",
+    "normal": "\033[22m",
+    "semitalico": "\033[23m",
+    "sem_sublinhado": "\033[24m",
+    "sem_inverso": "\033[27m",
+    "visivel": "\033[28m",
+    "sem_tachado": "\033[29m"
+}
+frase = "BOLETIM DE DICIONÁRIO"
+
+print(f"{estilos['negrito']}{cores['azul']}{"==="*5}{cores['cinza']}NOTAS{cores['verde']}{"==="*5}{cores['limpa']}")
+print(f"{cores['cinza']}{fundo['branco']}{estilos['negrito']}{frase.center(35)}{cores['limpa']}")
+print(f"{estilos['negrito']}{cores['vermelho']}{"==="*11+"=="}{cores['limpa']}") 
+
+
 def notas(*resp):
     resultado = {
         "Total: ": len(resp),
@@ -6,7 +60,38 @@ def notas(*resp):
         "Média: ": sum(resp) / len(resp)
     }
     return resultado
-
+#NãO DA PRA PERSONALIZAR
 
 resp = notas(5.5, 9.5, 10, 6.5)
+print(resp)
+
+
+
+solution = "SOLUÇÃO DO GUANÁ"
+
+print(f"{estilos['negrito']}{cores['ciano']}{"==="*5}{cores['cinza']}GUANÁ{cores['ciano']}{"==="*5}{cores['limpa']}")
+print(f"{cores['pretoebranco']}{estilos['negrito']}{solution.center(35)}{cores['limpa']}")
+print(f"{estilos['negrito']}{cores['ciano']}{"==="*11+"=="}{cores['limpa']}")
+
+def notas(*n, sit=False):
+    r = dict()
+    r['total'] = len(n),
+    r['maior'] = max(n),
+    r['menor'] = min(n),
+    r['media'] = sum(n) / len(n)
+
+    if sit:
+        if r['media'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['media'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+
+
+    return r
+
+
+#programa principal
+resp = notas(9, 10, 5.5, 2.5, 9, 8.5, sit=True)
 print(resp)
