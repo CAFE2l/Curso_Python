72d73525ade184a811b031c3f7455a7fdab29f44
CAFE2l <pciskolargx@gmail.com>
2025-09-20 13:17:34 -0300

    ex106 with solution

--- Exercicios/ex106/index.py
+++ Exercicios/ex106/index.py
@@ -1,8 +1,77 @@
 # Certifique-se de que moedas.py está no mesmo diretório ou ajuste o caminho conforme necessário
 from modulo.moedas import metade, dobro, aumentar, diminuir
 
+cores = {
+    "limpa": "\033[m",
+    'vermelho': "\033[31m",
+    'verde': "\033[32m",
+    'amarelo': "\033[33m",
+    'azul': "\033[34m",
+    'roxo': "\033[35m",
+    'ciano': "\033[36m",
+    'cinza': "\033[37m",
+    'pretoebranco': '\033[7;30m'
+}
+
+fundo = {
+    "branco": "\033[40m",
+    'vermelho': "\033[41m",
+    'verde': "\033[42m",
+    'amarelo': "\033[43m",
+    'azul': "\033[44m",
+    'roxo': "\033[45m",
+    'ciano': "\033[46m",
+    'cinza': "\033[47m",
+    'vermelho_claro': '\033[101m',
+    'verde_claro': '\033[102m',
+    'amarelo_claro': '\033[103m',
+    'azul_claro': '\033[104m',
+    'roxo_claro': '\033[105m',
+    'ciano_claro': '\033[106m',
+    'cinza_claro': '\033[107m'
+}
+
+estilos = {
+    "reset": "\033[0m",
+    "negrito": "\033[1m",
+    "fraco": "\033[2m",
+    "italico": "\033[3m",
+    "sublinhado": "\033[4m",
+    "inverso": "\033[7m",
+    "invisivel": "\033[8m",
+    "tachado": "\033[9m",
+    "duplosublinhado": "\033[21m",
+    "normal": "\033[22m",
+    "semitalico": "\033[23m",
+    "sem_sublinhado": "\033[24m",
+    "sem_inverso": "\033[27m",
+    "visivel": "\033[28m",
+    "sem_tachado": "\033[29m"
+}
+
+
 
 print(metade(80))
 print(dobro(80))
 print(aumentar(80))
 print(diminuir(80))
+
+
+
+
+
+solution = "SOLUÇÃO DO GUANÁ"
+
+print(f"{estilos['negrito']}{cores['ciano']}{"==="*5}{cores['cinza']}GUANÁ{cores['ciano']}{"==="*5}{cores['limpa']}")
+print(f"{cores['pretoebranco']}{estilos['negrito']}{solution.center(35)}{cores['limpa']}")
+print(f"{estilos['negrito']}{cores['ciano']}{"==="*11+"=="}{cores['limpa']}")
+
+
+from modulo import moedas2 
+
+t = int(input("Digite a taxa de aumento (%): "))
+p = float(input("Digite o preço: R$"))
+print(f"A metade de {p} é {moedas2.metade(p)}")
+print(f"O dobro de {p} é {moedas2.dobro(p)}")
+print(f"Aumentado {t}% de {p} é {moedas2.aumentar(p, t)}")
+print(f"Diminuido {t}% de {p} é {moedas2.diminuir(p, t)}")
