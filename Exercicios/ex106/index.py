18faf5e647b3c6f79b496ed876f71836e72f6a36
CAFE2l <pciskolargx@gmail.com>
2025-09-20 12:58:10 -0300

    ex106 solved

---
+++ Exercicios/ex106/index.py
@@ -0,0 +1,8 @@
+# Certifique-se de que moedas.py está no mesmo diretório ou ajuste o caminho conforme necessário
+from modulo.moedas import metade, dobro, aumentar, diminuir
+
+
+print(metade(80))
+print(dobro(80))
+print(aumentar(80))
+print(diminuir(80))
---
+++ Exercicios/ex106/modulo/moedas.py
@@ -0,0 +1,14 @@
+def aumentar(a):
+    return a + 1
+
+
+def diminuir(a):
+    return a - 1
+
+
+def dobro(a):
+    return a * 2 
+
+
+def metade(a):
+    return a/2
